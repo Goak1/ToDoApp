16e2d38c77bbc323624164d96220720b5282a34a
VictorDias2023274 <82916934+Goak1@users.noreply.github.com>
2025-07-16 18:33:40 +0100

    Basic code for the manager app

---
+++ ToDoApp/app/(tabs)/Style/TaskStyle.tsx
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native'
+
+export const style = StyleSheet.create({
+  container:{
+    flex: 1, 
+    flexDirection: 'column',
+  },
+
+  text:{
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign:'center',
+  },
+
+  taskText:{
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign:'center',
+  },
+
+  profileView:{
+
+  },
+
+  UserProfile:{
+
+  },
+
+  profileDetail:{
+
+  },
+  
+})
